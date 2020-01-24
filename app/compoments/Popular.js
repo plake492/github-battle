bb8b373305f6a649d7886e3f893e97665c5ff838
plake <plake492@gmail.com>
2020-01-24 14:30:50 -0500

    removed hoc, replaced with render prop

--- app/compoments/Popular.js
+++ app/compoments/Popular.js
@@ -9,6 +9,7 @@ import {
 } from "react-icons/Fa";
 import Card from "./Card";
 import Loading from "./Loading";
+import Tooltip from "./Tooltip";
 
 function LanguagesNav({ selected, onUpdatedLanguage }) {
   const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
@@ -59,8 +60,10 @@ function ReposGrid({ repos }) {
             >
               <ul className="card-list">
                 <li>
-                  <FaUser color="rgb(255,191,116)" size={22} />
-                  <a href={`https://github.com/${login}`}>{login}</a>
+                  <Tooltip text="Github username">
+                    <FaUser color="rgb(255,191,116)" size={22} />
+                    <a href={`https://github.com/${login}`}>{login}</a>
+                  </Tooltip>
                 </li>
                 <li>
                   <FaStar color="rgb(255,215, 0)" size={22} />
